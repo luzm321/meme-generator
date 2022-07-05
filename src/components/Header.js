0ff6be40fd7df96fc8de6f100a4015f8bcbbf0db
Luz Angelique Madrazo <luzm321@yahoo.com>
2022-07-05 15:49:41 -0500

    added header component

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+//import React from "react"
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img 
+                src="https://i.kym-cdn.com/entries/icons/original/000/000/091/TrollFace.jpg" 
+                className="header--image"
+            />
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--project">Meme-worthy posts!</h4>
+        </header>
+    )
+};
+
+export default Header;
